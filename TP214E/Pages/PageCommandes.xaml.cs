5f2ba699c9436bf515b054ce541e97c59004c653
perenperen <daniel.parent@hotmail.com>
2021-10-28 16:10:19 -0400

    correction window vers page

--- TP214E/Pages/PageCommandes.xaml.cs
+++ TP214E/Pages/PageCommandes.xaml.cs
@@ -8,14 +8,15 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace TP214E.Pages
 {
     /// <summary>
-    /// Logique d'interaction pour PageCommandes.xaml
+    /// Interaction logic for PageCommandes.xaml
     /// </summary>
-    public partial class PageCommandes : Window
+    public partial class PageCommandes : Page
     {
         public PageCommandes()
         {
